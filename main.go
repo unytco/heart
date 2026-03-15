07593a5f25ccbe09130f21ec689ad3ff47fa7779
ThetaSinner <ThetaSinner@users.noreply.github.com>
2026-03-15 02:38:18 +0000

    chore: fixes

--- main.go
+++ main.go
@@ -100,7 +100,7 @@ func main() {
 					Hour:    pulumi.Int(8),
 				},
 				UserData: pulumi.String(defaultCloudInit),
-			})
+			}, pulumi.IgnoreChanges([]string{"sshKeys", "userData"}))
 			if err != nil {
 				return err
 			}
@@ -138,7 +138,7 @@ func main() {
 				Monitoring: pulumi.Bool(true),
 				Backups:    pulumi.Bool(false),
 				UserData:   pulumi.String(defaultCloudInit),
-			})
+			}, pulumi.IgnoreChanges([]string{"sshKeys", "userData"}))
 			if err != nil {
 				return err
 			}
@@ -176,7 +176,7 @@ func main() {
 				Monitoring: pulumi.Bool(true),
 				Backups:    pulumi.Bool(false),
 				UserData:   pulumi.String(defaultCloudInit),
-			})
+			}, pulumi.IgnoreChanges([]string{"sshKeys", "userData"}))
 			if err != nil {
 				return err
 			}
